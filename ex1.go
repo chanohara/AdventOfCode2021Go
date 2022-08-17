3d34a947b4eeb9b180495ce2a733d1026f37171d
Dennis Gauss <chan05@hotmail.de>
2022-08-17 12:00:30 +0200

    ex1a solution

--- ex1.go
+++ ex1.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
+	data, err := os.ReadFile("input_ex1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	string := string(data)
+	string2 := strings.Split(string, "\n")
+	prev := 0
+	count := 0
+	for i, s := range string2 {
+		currInt, _ := strconv.Atoi(s)
+		if i > 0 {
+			if prev < currInt {
+				count += 1
+			}
+		}
+		prev = currInt
+	}
+	fmt.Print(count)
 }
